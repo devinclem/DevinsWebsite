1a57a9a7067420116e9962457686f741b5a06119
Devin Clemons <devincle@buffalo.edu>
2023-02-18 17:19:35 -0500

    Tutorial 12

--- src/home.js
+++ src/home.js
@@ -4,14 +4,15 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const[blogs, setBlogs] = useState([
-        {title: "My new website", body: "Bruh", author: "Devin", id: 1},
+        {title: "My new website", body: "Bruh", author: "Jerp", id: 1},
         {title: "I like hot dogs", body: "hot dogs re my favorite", author: "Devin", id: 2},
-        {title: "My new ", body: "Bruh", author: "Devin", id: 3},
+        {title: "My new ", body: "Bruh", author: "Seven", id: 3},
     ]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs}title ="All Messages"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "Devin")} title ="Devins Blogs"/>
         </div>
      );
 }
