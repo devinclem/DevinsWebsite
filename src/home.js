e67e032d7328cbf94f15a452963671ce59bc5b78
Devin Clemons <devincle@buffalo.edu>
2023-02-18 22:21:10 -0500

    Idk what tutorial i forgot

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './index.css';
 import BlogList from "./BlogList";
 
@@ -8,14 +8,25 @@ const Home = () => {
         {title: "I like hot dogs", body: "hot dogs re my favorite", author: "Devin", id: 2},
         {title: "My new ", body: "Bruh", author: "Seven", id: 3},
     ]);
+
+    const [name, setName] = useState("Devin");
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
-        console.log(newBlogs)
+        
     }
+
+    useEffect(() => {
+        console.log("Use effect ran")
+        console.log(blogs)
+    }, [name]);
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title ="All Messages" handleDelete={handleDelete}/>
+        <button onClick ={() => setName("Cheeseball")}>Change Name</button>
+        {name}
         </div>
      );
 }
